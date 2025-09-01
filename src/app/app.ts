0292a24eb48561dfeb12372f610befd17ab3dace
Victor Cantanhede <victorcantanhedblk@gmail.com>
2025-09-01 12:05:26 -0300

    feat: routes created

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Login } from './login/login';
-import { Card01 } from './components/ui/card-01/card-01';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Login],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
